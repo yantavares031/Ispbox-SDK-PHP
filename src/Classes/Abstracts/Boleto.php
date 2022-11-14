2f98f4bf312ac88694fcf638869b05450276f3dc
Yan Tavares <yantavares021@hotmail.com>
2022-11-14 10:16:59 -0300

    Add Settle Method in Boleto.php

--- src/Classes/Abstracts/Boleto.php
+++ src/Classes/Abstracts/Boleto.php
@@ -3,21 +3,24 @@ namespace Ispbox2\Abstracts;
 use Ispbox2\Classes\Json;
 use Ispbox2\Enums\Contratos\Tipo;
 use Ispbox2\Enums\financeiro\DocStatus;
+use Ispbox2\Enums\financeiro\DocTipo;
+use Ispbox2\Http\RestClient;
+use Ispbox2\SDK;
 
 abstract class Boleto{
-    public string  $documentoId;
-    public string  $clienteId;
-    public string  $contratoId;
-    public string  $descricao;
-    public string  $dataGeracao;
-    public string  $vencimento;
-    public string  $valor;
-    public float   $valorFloat;
-    public string  $valorPago;
-    public string  $dataPagamento;
-    public Tipo    $tipoContrato;
-    public DocStatus  $status;
-    public bool    $atrasado;
+    public string    $documentoId;
+    public string    $clienteId;
+    public string    $contratoId;
+    public string    $descricao;
+    public string    $dataGeracao;
+    public string    $vencimento;
+    public string    $valor;
+    public float     $valorFloat;
+    public string    $valorPago;
+    public string    $dataPagamento;
+    public Tipo      $tipoContrato;
+    public DocStatus $status;
+    public bool      $atrasado;
 
     public function __construct(){
 
@@ -39,5 +42,20 @@ abstract class Boleto{
         $this->status        = DocStatus::from($stdobj->status);
         $this->atrasado      = ($stdobj->vencido == 0) ? false : true;
         $this->dataPagamento = ($this->status == DocStatus::Pago) ? $parseDate($stdobj->data_pagamento) : '';
+    }
+
+    public function settle(string $dataPagamento){
+        if($this->status == DocStatus::Aberto)
+        {
+            $Rest     = new RestClient(SDK::$Host);
+            $Rest->setBasicAuth(SDK::AuthString());
+            $JsonRecibo = $Rest->Post('/clientes_cobrancas/liquidar',[
+                'id'                   => $this->documentoId,
+                'valor_pago'           => $this->valor,
+                'data_pagamento'       => $this->dataPagamento,
+                'forma_recebimento_id' => 3,
+                'gerar_recibo'         => 0,
+            ]);
+        }
     }
 }
