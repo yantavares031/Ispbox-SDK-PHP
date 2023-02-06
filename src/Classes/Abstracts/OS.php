8e4f009a247a9e9beb86fef84b7b565705c19afe
Yan Tavares <yantavares021@hotmail.com>
2023-02-06 14:00:44 -0300

    update

--- src/Classes/Abstracts/OS.php
+++ src/Classes/Abstracts/OS.php
@@ -1,42 +1,94 @@
 <?php
-namespace Ispbox2\Classes\Abstracts;
-use Ispbox2\Classes\Endereco;
+namespace Ispbox2;
+
+use Exception;
 use Ispbox2\Classes\Json;
-use Ispbox2\Enums\OS\OStatus;
+use Ispbox2\Http\RestClient;
+use Ispbox2\Classes\Abstracts\OS;
 
-abstract class OS{
-    
-    public bool     $agendada;
-    public int      $os_id;
-    public string   $cliente_nome;
-    public string   $tipo;
-    public string   $hora_agendamento;
-    public string   $hora_abertura;
-    public string   $hora_fechamento;
-    public string   $responsavel;
-    public OStatus  $status;
-    public Endereco $endereco;
+class Ordens{
+    private array $ordens = [
+        "Agendadas"    => [],
+        "NaoAgendadas" => []
+    ];
 
-    function __construct(){
-        $this->endereco = new Endereco();
+    public function __construct(){
+
+        $agendadas    = $this->getAgendadas();
+        $naoAgendadas = $this->getNaoAgendadas();
+        $Merge        = array_merge($agendadas->rows, $naoAgendadas->rows);
+
+        foreach($Merge as $key => $obj){
+            $cell = $obj->cell;
+            $os = (count($cell) == 14) ? new Agendadas() : new NaoAgendadas();
+            $os->fromObject($cell);
+            $arrKeyName = $this->ShortClass($os);
+            array_push($this->ordens[$arrKeyName], $os);
+        }
     }
 
-    function fromObject($stdobj){
-        $parseDate = fn($date) => date('d/m/Y',strtotime($date));
-        
-        // $this->os_id                 = $stdobj->id;
-        // $this->nome                  = $stdobj->nome;
-        // $this->dataCadastro          = $parseDate($stdobj->data_cadastro);
-        // $this->telefone              = $stdobj->telefone;
-        // $this->celular               = $stdobj->celular;
-        // $this->email                 = $stdobj->email;
-        // $this->emailSecundario       = $stdobj->email2;
-        // $this->endereco->logradouro  = $stdobj->endereco;
-        // $this->endereco->numero      = $stdobj->endereco_numero;
-        // $this->endereco->bairro      = $stdobj->endereco_bairro;
-        // $this->endereco->cidade      = $stdobj->cidade;
-        // $this->endereco->estado      = $stdobj->uf;
-        // $this->endereco->complemento = $stdobj->endereco_complemento;
-        // $this->endereco->cep         = $stdobj->cep;
+    private function getAgendadas() : Json{
+        $Rest     = new RestClient(SDK::$host);
+        $Rest->setBasicAuth(SDK::authString());
+        $Agendadas = $Rest->Post('/ordensservicos/index',[
+            '_search'           => 'false',
+            'rows'              => '999999',
+            'page'              => 1,
+            'sidx'              => 'data',
+            'sord'              => 'desc',
+            'agendada'         => 1,
+            'numero_os'         => '',
+            'data_inicial'      => '01/01/2022',
+            'data_final'        => '31/12/2023',
+            'agendamento_final' => '',
+            'status'            => '-1',
+            'origem_tipo'       => 'todas',
+            'setores_id'        => -1,
+            'tecnico_id'        => -1,
+            'clientes_id'       => '',
+            'ordens_servicos_tipos_id' => -1,
+            'motivo_contrato_id'       => -1,
+            'solucao_id'        => -1,
+            'condominios_id'    => ''
+        ]);
+
+        return $Agendadas;
+    }
+
+    private function getNaoAgendadas() : Json{
+        $Rest     = new RestClient(SDK::$host);
+        $Rest->setBasicAuth(SDK::authString());
+        $NAgendadas = $Rest->Post('/ordensservicos/index',[
+            '_search'           => 'false',
+            'rows'              => '999999',
+            'page'              => 1,
+            'sidx'              => 'data',
+            'sord'              => 'desc',
+            'agendada'         => 0,
+            'numero_os'         => '',
+            'data_inicial'      => '01/01/2022',
+            'data_final'        => '31/12/2023',
+            'agendamento_final' => '',
+            'status'            => '-1',
+            'origem_tipo'       => 'todas',
+            'setores_id'        => -1,
+            'tecnico_id'        => -1,
+            'clientes_id'       => '',
+            'ordens_servicos_tipos_id' => -1,
+            'motivo_contrato_id'       => -1,
+            'solucao_id'        => -1,
+            'condominios_id'    => ''
+        ]);
+
+        return $NAgendadas;
+    }
+
+    public function takeAll() : array{
+        return $this->ordens; 
+    }
+
+    private function ShortClass($class) : string{
+        $arrClass = explode('\\',get_class($class)); 
+        return $arrClass[count($arrClass)-1];
     }
 }
